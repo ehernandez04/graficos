745a4ed6436ce026e87d1bd2eec01d677990fb86
Erick Hernandez <eleonel@audifilm.com>
2024-03-13 16:42:47 +0100

    Actualizando el servicio

--- src/app/google-chart/service/multi-chart-data.service.ts
+++ src/app/google-chart/service/multi-chart-data.service.ts
@@ -1,15 +1,41 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class MultiChartDataService {
 
+export class MultiChartDataService {
+  chartData = [
+    [
+        "London",
+        8136000
+    ],
+    [
+        "New York",
+        8538000
+    ],
+    [
+        "Paris",
+        2244000
+    ],
+    [
+        "Berlin",
+        3470000
+    ],
+    [
+        "Kairo",
+        19500000
+    ]
+  ]
   constructor(private httpClient: HttpClient) { }
 
   getChartData(): Observable<Array<Array<string | number>>>{
-    return this.httpClient.get<Array<Array<string | number>>>('http://localhost:3000/chartData');
+   // return this.httpClient.get<Array<Array<string | number>>>('http://localhost:3000/chartData');
+
+    return of(this.chartData);
   }
 }
